c77df04489d7ee6b00d2d00508f0c2d1e7e3a37b
Igor Souza <igormcsouza@gmail.com>
2021-06-26 20:57:19 -0300

    Update code basis

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>My Blog! :D</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
