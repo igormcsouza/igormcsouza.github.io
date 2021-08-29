9d3df4c7532ced7e38d3443c8d7f75d9bd7be2b1
Igor Souza <igormcsouza@gmail.com>
2021-08-29 12:04:13 -0300

    Add function to calculate my age

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function calculate_age(dob) {
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms);
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+document.getElementById('my-age').innerHTML = calculate_age(new Date(1994, 9, 9))
