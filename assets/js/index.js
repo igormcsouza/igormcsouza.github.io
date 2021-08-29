96272d1fe962eaf46a1eebe85ba98995b21f82c8
Igor Souza <igormcsouza@gmail.com>
2021-08-29 12:16:29 -0300

    Comment js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,10 @@
+// Add my age according to the current date
+
 function calculate_age(dob) {
     var diff_ms = Date.now() - dob.getTime();
     var age_dt = new Date(diff_ms);
     return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
 
-document.getElementById('my-age').innerHTML = calculate_age(new Date(1994, 9, 9))
+document.getElementById('my-age').innerHTML = calculate_age(
+    new Date(1994, 9, 9))
