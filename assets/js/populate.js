050808e832704d25413d36869274b54f8575d982
Igor Souza <igormcsouza@gmail.com>
2023-01-05 12:04:50 -0300

    Add my company info

--- assets/js/populate.js
+++ assets/js/populate.js
@@ -3,3 +3,5 @@ import data from '/assets/js/data.json' assert { type: "json" };
 document.getElementById('personal.bio').innerText = data.personal.bio
 document.getElementById('work.linkedin').href = data.work.linkedin
 document.getElementById('work.workplace').innerText = data.work.workplace
+document.getElementById('company.name').innerText = data.company.name
+document.getElementById('company.website').href = data.company.website
